e113a8b9d897c9779417c6ce10b72d8d0ab71446
itaizy <itaizy@163.com>
2020-02-10 23:52:56 +0800

    信息汇总添加

--- src/App.js
+++ src/App.js
@@ -113,14 +113,17 @@ function Summary () {
   return (
     <div className="card info">
       <h2 id="Summary">信息汇总</h2>
+      <li><a href="http://2019ncov.nosugartech.com/">确诊患者同行查询工具</a></li>
       <li>
         <a href="https://m.yangshipin.cn/static/2020/c0126.html">疫情24小时 | 与疫情赛跑</a>
       </li>
-      <li><a href="http://2019ncov.nosugartech.com/">确诊患者同行查询工具</a></li>
       <li><a href="https://news.qq.com/zt2020/page/feiyan.htm">腾讯新闻新冠疫情实时动态</a></li>
       <li><a href="https://3g.dxy.cn/newh5/view/pneumonia">丁香园新冠疫情实时动态</a></li>
       <li><a href="https://vp.fact.qq.com/home">新型冠状病毒实时辟谣</a></li>
       <li><a href="https://promo.guahao.com/topic/pneumonia">微医抗击疫情实时救助</a></li>
+      <li><a href="http://www.ncov-report.com/">疫情评估与预测报告-北航</a></li>
+      <li><a href="http://ncov.deepeye.tech/">疫情可视化分析-清华</a></li>
+      <li><a href="http://vis.pku.edu.cn/ncov/">疫情可视化分析-北大</a></li>
     </div>
   )
 }
@@ -195,7 +198,7 @@ function StatIncr ({ modifyTime}) {
         <TotalTag number={all.deadIncr} total={all.deadCount} className="dead">
           死亡
         </TotalTag>
-        <TotalTag number={all.curedIncr} total={all.curedIncr} className="numbercured">
+        <TotalTag number={all.curedIncr} total={all.curedCount} className="numbercured">
           治愈
         </TotalTag>
       </div>
