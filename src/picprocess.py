5f9b062c7b3ba3d08487ddf9dbc63a26c88dd31e
ubuntu <itaizy@163.com>
2020-02-13 20:24:34 +0800

    新闻去重

--- src/picprocess.py
+++ src/picprocess.py
@@ -7,6 +7,20 @@ import numpy as np
 val = os.system('cd .. && node scripts/build-origin.js')
 print('DXY Data update.' + str(val))
 val = os.system('wget -O - http://c.m.163.com/nc/article/headline/T1348647853363/0-40.html > data/n163.json')
+f163 = open('data/n163.json',encoding='utf-8')
+news = json.load(f163)
+f163.close()
+newsDict = {}
+for new in news['T1348647853363']:
+    newsDict[new['docid']] = new
+newsf = []
+for new in newsDict:
+    newsf.append(newsDict[new])
+newsfw = {}
+newsfw['T1348647853363'] = newsf
+with open('./data/n163.json', 'w', encoding='utf-8') as fp:
+    json.dump(newsfw, fp, ensure_ascii=False)
+fp.close()
 print('n163 Data update.' + str(val))
 f = open('data/overall.json',encoding='utf-8')
 user_dic = json.load(f)
